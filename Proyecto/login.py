6beed6236c6b1095011f5074b671db6ec41bcd49
Pritschella <prits99@hotmail.com>
2021-05-20 11:57:17 -0500

    Commit: "Se creó la ventana Registro"

--- Proyecto/login.py
+++ Proyecto/login.py
@@ -7,7 +7,7 @@ Created on 20 may. 2021
 from tkinter import *
 import os
 
-#CREAMOS VENTANA PRINCIPAL.
+#VENTANA PRINCIPAL.
 def ventana_inicio():
     global ventana_principal
     pestas_color="DarkGrey"
@@ -21,3 +21,33 @@ def ventana_inicio():
     Button(text="Registrarse", height="2", width="30", bg=pestas_color, command=registro).pack() #BOTÓN "Registrarse".
     Label(text="").pack()
     ventana_principal.mainloop()
+    
+#VENTANA PARA REGISTRO.
+def registro():
+    global ventana_registro
+    ventana_registro = Toplevel(ventana_principal)
+    ventana_registro.title("Registro")
+    ventana_registro.geometry("300x250")
+ 
+    global nombre_usuario
+    global clave
+    global entrada_nombre
+    global entrada_clave
+    nombre_usuario = StringVar() #DECLARAMOS "string" COMO TIPO DE DATO PARA "nombre_usuario"
+    clave = StringVar() #DECLARAMOS "sytring" COMO TIPO DE DATO PARA "clave"
+ 
+    Label(ventana_registro, text="Introduzca datos", bg="LightGreen").pack()
+    Label(ventana_registro, text="").pack()
+    etiqueta_nombre = Label(ventana_registro, text="Nombre de usuario * ")
+    etiqueta_nombre.pack()
+    entrada_nombre = Entry(ventana_registro, textvariable=nombre_usuario) #ESPACIO PARA INTRODUCIR EL NOMBRE.
+    entrada_nombre.pack()
+    etiqueta_clave = Label(ventana_registro, text="Contrasena * ")
+    etiqueta_clave.pack()
+    entrada_clave = Entry(ventana_registro, textvariable=clave, show='*') #ESPACIO PARA INTRODUCIR LA CONTRASEÑA.
+    entrada_clave.pack()
+    Label(ventana_registro, text="").pack()
+    Button(ventana_registro, text="Registrarse", width=10, height=1, bg="LightGreen", command = registro_usuario).pack() #BOTÓN "Registrarse"
+    
+    
+    
