2bbc7e773792b954486eb2250734f504c3713256
Pritschella <prits99@hotmail.com>
2021-05-24 14:15:02 -0500

    Commit: "Se modificó login"

--- Proyecto/conexion.py
+++ Proyecto/conexion.py
@@ -5,11 +5,14 @@ Created on 22 may. 2021
 '''
 
 import pymysql
+
 try:
     conexion = pymysql.connect(host='localhost',
                                user='root',
                                password='berenice',
                                db='employees')
+    
     print("Conexion correcta")
+
 except(pymysql.err.OperationalError, pymysql.err.InternalError) as e:
     print("Ocurrio un error al conectar: ", e)
--- Proyecto/login.py
+++ Proyecto/login.py
@@ -4,156 +4,59 @@ Created on 20 may. 2021
 @author: prits
 '''
 
-from tkinter import *
-import os
+import tkinter as tk
+from tkinter import messagebox as MB
 
-#VENTANA PRINCIPAL.
-def ventana_inicio():
-    global ventana_principal
-    pestas_color="DarkGrey"
-    ventana_principal=Tk()
-    ventana_principal.geometry("300x250")#DIMENSIONES DE LA VENTANA
-    ventana_principal.title("Login con tkinter")
-    Label(text="Selecciona una opcion", bg="LightGreen", width="300", height="2", font=("Calibri", 13)).pack()
-    Label(text="").pack()
-    Button(text="Acceder", height="2", width="30", bg=pestas_color, command=login).pack() 
-    Label(text="").pack()
-    Button(text="Registrarse", height="2", width="30", bg=pestas_color, command=registro).pack() 
-    Label(text="").pack()
-    ventana_principal.mainloop()
+#Creacion login y configuracion
+login = tk.Tk()
+login.title("Login")
+login.geometry("3000x300")
+login.configure(bg="SkyBlue")
+
+#Titulo
+titulo = tk.Label(login, text="SISTEMA DE EMPLEADOS", bg="SkyBlue", font=("Helvetica", 16))
+titulo.pack(fill=tk.X)
+etiqueta=tk.Label(login, text="Llena los siguientes campos", bg = "SkyBlue")
+etiqueta.pack()
+
+#etiquetas
+usLabel = tk.Label(login, text="Usuario", bg = "SkyBlue")
+usLabel.place(x=20, y=80)
+conLabel = tk.Label(login, text="Password", bg="SkyBlue")
+conLabel.place(x=20, y=180)
+
+#cajas
+usCaja = tk.Entry(login)
+usCaja.place(x=100, y=80)
+conCaja = tk.Entry(login, show="*")
+conCaja.place(x=100, y =180)
+
+#boton
+btnIniciar=tk.Button(login, text="        Iniciar        ", height=2,  command=lambda: obtenerDatos())
+btnIniciar.place(x=110, y=250)
+
+#vars
+strUs=tk.StringVar()
+strCon=tk.StringVar()
+
+#metodos
+def obtenerDatos():
+    strUs.set(usCaja.get())
+    strCon.set(conCaja.get())
     
-#VENTANA PARA REGISTRO.
-def registro():
-    global ventana_registro
-    ventana_registro = Toplevel(ventana_principal)
-    ventana_registro.title("Registro")
-    ventana_registro.geometry("300x250")
- 
-    global nombre_usuario
-    global clave
-    global entrada_nombre
-    global entrada_clave
-    nombre_usuario = StringVar() 
-    clave = StringVar() 
- 
-    Label(ventana_registro, text="Introduzca datos", bg="LightGreen").pack()
-    Label(ventana_registro, text="").pack()
-    etiqueta_nombre = Label(ventana_registro, text="Nombre de usuario * ")
-    etiqueta_nombre.pack()
-    entrada_nombre = Entry(ventana_registro, textvariable=nombre_usuario) 
-    entrada_nombre.pack()
-    etiqueta_clave = Label(ventana_registro, text="Contrasena * ")
-    etiqueta_clave.pack()
-    entrada_clave = Entry(ventana_registro, textvariable=clave, show='*') 
-    entrada_clave.pack()
-    Label(ventana_registro, text="").pack()
-    Button(ventana_registro, text="Registrarse", width=10, height=1, bg="LightGreen", command = registro_usuario).pack()
-    
-#VENTANA PARA LOGIN.
-def login():
-    global ventana_login
-    ventana_login = Toplevel(ventana_principal)
-    ventana_login.title("Acceso a la cuenta")
-    ventana_login.geometry("300x250")
-    Label(ventana_login, text="Introduzca nombre de usuario y contrasena").pack()
-    Label(ventana_login, text="").pack()
- 
-    global verifica_usuario
-    global verifica_clave
- 
-    verifica_usuario = StringVar()
-    verifica_clave = StringVar()
- 
-    global entrada_login_usuario
-    global entrada_login_clave
- 
-    Label(ventana_login, text="Nombre usuario * ").pack()
-    entrada_login_usuario = Entry(ventana_login, textvariable=verifica_usuario)
-    entrada_login_usuario.pack()
-    Label(ventana_login, text="").pack()
-    Label(ventana_login, text="Contrasena * ").pack()
-    entrada_login_clave = Entry(ventana_login, textvariable=verifica_clave, show= '*')
-    entrada_login_clave.pack()
-    Label(ventana_login, text="").pack()
-    Button(ventana_login, text="Acceder", width=10, height=1, command = verifica_login).pack()
-    
-#VENTANA "VERIFICACION DE LOGIN".
-def verifica_login():
-    usuario1 = verifica_usuario.get()
-    clave1 = verifica_clave.get()
-    entrada_login_usuario.delete(0, END) 
-    entrada_login_clave.delete(0, END) 
- 
-    lista_archivos = os.listdir() 
-    
-    if usuario1 in lista_archivos:
-        archivo1 = open(usuario1, "r")
-        verifica = archivo1.read().splitlines() 
-        
-        if clave1 in verifica:
-            exito_login() 
-            
-        else:
-            no_clave() 
+    if not strUs.get() or not strCon.get():
+        MB.showerror("Error", "Llena los campos")
+    elif not strUs.get().isalpha():
+        MB.showerror("Error", "Usuario Solo Puede Llevar LETRAS")
     else:
-        no_usuario() 
-    
-# VENTANA "Login finalizado con exito".
-def exito_login():
-    global ventana_exito
-    ventana_exito = Toplevel(ventana_login)
-    ventana_exito.title("Exito")
-    ventana_exito.geometry("150x100")
-    Label(ventana_exito, text="Login finalizado con exito").pack()
-    Button(ventana_exito, text="OK", command=borrar_exito_login).pack()
-    
-#VENTANA DE "Contrasena incorrecta".
-def no_clave():
-    global ventana_no_clave
-    ventana_no_clave = Toplevel(ventana_login)
-    ventana_no_clave.title("ERROR")
-    ventana_no_clave.geometry("150x100")
-    Label(ventana_no_clave, text="Contrasena incorrecta ").pack()
-    Button(ventana_no_clave, text="OK", command=borrar_no_clave).pack()
-    
-#VENTANA DE "Usuario no encontrado". 
-def no_usuario():
-    global ventana_no_usuario
-    ventana_no_usuario = Toplevel(ventana_login)
-    ventana_no_usuario.title("ERROR")
-    ventana_no_usuario.geometry("150x100")
-    Label(ventana_no_usuario, text="Usuario no encontrado").pack()
-    Button(ventana_no_usuario, text="OK", command=borrar_no_usuario).pack()
-    
-#CERRADO DE VENTANAS
-def borrar_exito_login():
-    ventana_exito.destroy()
- 
- 
-def borrar_no_clave():
-    ventana_no_clave.destroy()
- 
- 
-def borrar_no_usuario():
-    ventana_no_usuario.destroy()
-    
-    
-#REGISTRO USUARIO    
-def registro_usuario():
- 
-    usuario_info = nombre_usuario.get()
-    clave_info = clave.get()
- 
-    file = open(usuario_info, "w")
-    file.write(usuario_info + "\n")
-    file.write(clave_info)
-    file.close()
- 
-    entrada_nombre.delete(0, END)
-    entrada_clave.delete(0, END)
- 
-    Label(ventana_registro, text="Registro completado con exito", fg="green", font=("calibri", 11)).pack()
- 
-ventana_inicio() 
- 
+        if strUs.get()=="a" and strCon.get()=="a":
+            abrirVentana()
+        else:
+            MB.showerror("Error", "Usuario o Contrasena Incorrectos")
+        
+def abrirVentana():
+    login.destroy()
+
+#--------------------------
+login.mainloop()
  
